400ffed559ee4e53896ae810166d5a1d115972b0
Aarav414 <84316227+Aarav414@users.noreply.github.com>
2021-10-29 19:09:27 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
-var SpeechRecognition = window.webkitSpeechRegocnition;
-var Recongnition = new SpeechRegocnition();
+var SpeechRecognition = window.webkitSpeechRecognition;
 
-function Start(){
-    document.getElementById("textbox").innerHTML="";
-    Recongnition.start();
+
+var recognition = new SpeechRecognition();
+
+function start() {
+    document.getElementById("textbox").innerHTML = "";
+    recognition.start();
 }
-recongnition.onresult = function (event);
-console.log(event);
-var Content = event.results[0][0].transcript;
-document.getElementById("textbox").innerHTML=Content;
+recognition.onresult = function (event) {
+    console.log(event);
+    var Content = event.results[0][0].transcript;
+    document.getElementById("textbox").innerHTML = Content;
+    console.log(Content);
+    speak();
+}
+
+function speak() {
+    var synth = window.speechSynthesis;
+    speak_data = document.getElementById("textbox").value;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+    Webcam.attach(camera);
+}
+Webcam.set({
+    width: 360,
+    height: 250,
+    image_format: 'png',
+    png_quality: 90
+});
+camera = document.getElementById("camera");
